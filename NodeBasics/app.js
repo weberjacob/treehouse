c79354fe12a41e7cf2677f03306be6524f3eeab2
jacob weber <hello@jacobweber.net>
2018-01-17 07:41:45 -0600

    Simple call to Treehouse API printing JSON data.

--- NodeBasics/app.js
+++ NodeBasics/app.js
@@ -1,2 +1,19 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
+const https = require('https');
+const username = "jacob";
+
+// Print message to console
+function printMessage(username, badgecount, points) {
+  const message = `${username} has ${badgecount} total badge(s) and ${points} points in Javascript`
+  console.log(message);
+}
+
+// Connect to API URL (https://teamtreehouse.com/username.json)
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+  console.log(response);
+  // Read the data
+  // Parse the data
+  // Print the data
+
+});
