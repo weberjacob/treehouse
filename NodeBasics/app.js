687708f35a9371a96012e23f0bbeb9420b29f4df
jacob weber <hello@jacobweber.net>
2018-01-17 07:16:45 -0600

    Removed React directory from Node Branch and setup for Node files.

---
+++ NodeBasics/app.js
@@ -0,0 +1,2 @@
+// Problem: We need a simple way to look at a user's badge count and JavaScript points
+// Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
