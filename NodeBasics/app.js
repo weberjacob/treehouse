6b4b68f0cf72d58d27292e85f988c343aea7d4b6
jacob weber <hello@jacobweber.net>
2018-01-19 07:23:03 -0600

    Setting up error handling and try-catch block.

--- NodeBasics/app.js
+++ NodeBasics/app.js
@@ -1,7 +1,7 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
-const username = "jacobweber";
+// const username = "jacobweber";
 
 // Print message to console
 function printMessage(username, badgecount, points) {
@@ -9,22 +9,37 @@ function printMessage(username, badgecount, points) {
   console.log(message);
 }
 
-// Connect to API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-  // console.log(response);
-  let body = "";
-  // Read the data
-  response.on('data', data => {
-    body += data.toString();
-  });
+function getProfile(username) {
 
-  response.on('end', () => {
-    // Parse the data
-    const profile = JSON.parse(body);
-    printMessage(username, profile.badges.length, profile.points.JavaScript);
-    // console.dir(profile);
-    // console.log(body);
-    // console.log(typeof body);
-    // Print the data
-  });
-});
+  try {
+    // Connect to API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      // console.log(response);
+      let body = "";
+      // Read the data
+      response.on('data', data => {
+        body += data.toString();
+      });
+
+      response.on('end', () => {
+        // Parse the data
+        const profile = JSON.parse(body);
+        printMessage(username, profile.badges.length, profile.points.JavaScript);
+        // console.dir(profile);
+        // console.log(body);
+        // console.log(typeof body);
+        // Print the data
+      });
+    });
+    request.on('error', error => console.error(`Problem with request: ${error.message}`));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// const users = ["chalkers", "jacobweber"];
+const users = process.argv.slice(2);
+users.forEach(getProfile);
+
+// getProfile("jacobweber");
+// getProfile("chalkers");
