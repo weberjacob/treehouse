4e2eac9bd04e3ee0adf2680702c92d0899b3a650
jacob weber <hello@jacobweber.net>
2018-01-22 07:21:13 -0600

    Updated error handling and http error messages.

--- NodeBasics/app.js
+++ NodeBasics/app.js
@@ -1,8 +1,11 @@
 // Problem: We need a simple way to look at a user's badge count and JavaScript points
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
-const https = require('https');
-// const username = "jacobweber";
 
+// Require http and https modules
+const https = require('https');
+const http = require('http');
+
+// const username = "jacobweber";
 // Print Error Message
 function printError(error) {
   console.error(error.message);
@@ -19,26 +22,32 @@ function getProfile(username) {
   try {
     // Connect to API URL (https://teamtreehouse.com/username.json)
     const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-      // console.log(response);
-      let body = "";
-      // Read the data
-      response.on('data', data => {
-        body += data.toString();
-      });
+      if (response.statusCode === 200) {
+        // console.log(response);
+        let body = "";
+        // Read the data
+        response.on('data', data => {
+          body += data.toString();
+        });
 
-      response.on('end', () => {
-        // Parse the data
-        try {
-          const profile = JSON.parse(body);
-          // Print the data
-          printMessage(username, profile.badges.length, profile.points.JavaScript);
-        } catch (error) {
-          printError(error);
-        }
-        // console.dir(profile);
-        // console.log(body);
-        // console.log(typeof body);
-      });
+        response.on('end', () => {
+          // Parse the data
+          try {
+            const profile = JSON.parse(body);
+            // Print the data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch (error) {
+            printError(error);
+          }
+          // console.dir(profile);
+          // console.log(body);
+          // console.log(typeof body);
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
     });
     request.on('error', error => console.error(`Problem with request: ${error.message}`));
   } catch (error) {
